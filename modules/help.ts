3aa63c5142871529abe00fdd6b8d21b4384ea803
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2021-12-19 21:14:03 -0500

    add more help commands

--- modules/help.ts
+++ modules/help.ts
@@ -17,6 +17,7 @@ export default function help(section: number): MessageEmbed {
             embed.setDescription("Hello, welcome to the help page! \n I've seperated the help page into groups to make it easier to navigate! \nTo access a group, do `-help #`, where the number is next to the group or command you want to access! \nI hope you find what you are looking for.")
             .setTitle("Help Main page")
             .addField("1 | Music", "Info about all the music commands!")
+            .addField("2 | Lights", "Info about all the light commands!")
             .addField("More soon!", "I will update this page when needed!")
 
             break
@@ -31,6 +32,18 @@ export default function help(section: number): MessageEmbed {
             .addField("103 | stop", "Stops playing, and leaves the VC")
             .addField("104 | queue", "Shows the list of songs that will play.")
             .addField("105 | find", "Searches a term on YouTube, and gets a link.")
+
+            break
+        }
+
+        case 2: {
+            //Light Page
+
+            embed.setDescription("Light commands! These are used to change the light settings!")
+            .addField("201 | l on", "Turns the light on.")
+            .addField("202 | l off", "Turns the light off.")
+            .addField("203 | l set", "Sets the light to a specific color.")
+            .addField("204 | l strobe", "Strobes the light between multiple colors.")
 
             break
         }
@@ -65,10 +78,31 @@ export default function help(section: number): MessageEmbed {
             break
         }
 
+        case 201: {
+            embed.setDescription("Turns the light on.\nAliases: `l on`")
+            .addField("Usage", "`-l on`")
+            break
+        }
+
+        case 202: {
+            embed.setDescription("Turns the light off.\nAliases: `l off`")
+            .addField("Usage", "`-l off`")
+            break
+        }
+
+        case 203: {
+            embed.setDescription("Sets the light to a specific color.\nAliases: `l set`")
+            .addField("Usage", "`-l set ffffff`\n`-l set 00ff00`\n`-l set 9337d0`\n`-l set b30000`")
+            break
+        }
+
+        case 204: {
+            embed.setDescription("Strobes the light between two colors.\nAliases: `l strobe`")
+            .addField("Usage", "`-l strobe ffffff 00ff00 `")
+            break
+        }
 
     }
-
-
     return embed
 
 }
